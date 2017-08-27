7d97bab3c3ea17b63dd9c4ec1f13d5bafb4b824c
swap-file <bigmarkslp@gmail.com>
2017-08-27 18:15:50 -0500

    Fix Launcher

--- go.sh
+++ go.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo Xset...
+xset s 0 0
 xset s off # don't activate screensaver
 xset s noblank # don't blank the video device
 xset s noexpose
-sudo startx ./glxgears
+xset -dpms
+./glxgears
 
